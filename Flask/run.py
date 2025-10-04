745215ad4eba4c6e0ce8dcbc721212d8294ae7d1
cnniranjan72 <cnniranjan72@gmail.com>
2025-10-04 14:58:23 +0530

    Fixed Errors

--- Flask/run.py
+++
@@ -1,9 +0,0 @@
-
-from app import create_app
-
-if __name__ == '__main__':
-    # Creates and configures the Flask app instance
-    app = create_app() 
-    
-    # Runs the application on the local server
-    app.run(debug=True)
